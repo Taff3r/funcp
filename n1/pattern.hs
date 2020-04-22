c36aaca7ccb626cae430ef163d71cb4ffb440609
Simon Tenggren <simon.tenggren@gmail.com>
2020-04-23 00:27:26 +0200

    work

--- n1/pattern.hs
+++ n1/pattern.hs
@@ -1,12 +1,25 @@
-{-
-match :: Eq a => a -> [a] -> [a] -> Maybe [a]
-match wc p  [] = Nothing
-match wc [] [] = []
-match wc [] p  = Nothing
--}
-
+import Data.List
 substitute :: Eq a => a -> [a] -> [a] -> [a] 
 substitute _ [] _  = []
 substitute wc (t:ts) s
           | wc == t = s ++ (substitute wc ts s)
           | otherwise = t : substitute wc ts s
+
+match :: Eq a => a -> [a] -> [a] ->  [a]
+match wc p  [] = [] 
+match wc [] s  = []
+match wc (p:pp) (s:ss)  
+            | s:ss == p:pp = []
+            | p == wc = takeUntil pp (s:ss)
+            | otherwise = match wc pp ss
+
+-- singleWildcardMatch :: Eq a => a -> [a] -> [a] -> Maybe [a]
+-- longerWildcardMatch :: Eq a => a -> [a] -> [a] -> Maybe [a]
+
+takeUntil :: Eq a => [a] -> [a] -> [a]
+takeUntil _ [] = []
+takeUntil a b
+            | a == b = [] 
+            | otherwise = head b : takeUntil a (tail b)
+
+

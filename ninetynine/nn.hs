e28c2bcbe19fd3659d3612d3f27776750e1b191c
Simon Tenggren <simon.tenggren@gmail.com>
2020-04-22 15:13:03 +0200

    stuff

--- ninetynine/nn.hs
+++ ninetynine/nn.hs
@@ -1,3 +1,4 @@
+import Data.List
 -- 1
 myLast::[a] -> a
 myLast [] = undefined 
@@ -23,3 +24,22 @@ myReverse::[a] -> [a]
 myReverse [a] = [a]
 myReverse [] = []
 myReverse (x:xs) = (myReverse xs) ++ [x]
+
+-- 6
+isPalindrome::(Eq a) => [a] -> Bool
+isPalindrome ls = (ls == (myReverse ls))
+
+-- 7
+data NestedList a = Elem a | List [NestedList a]
+flatten::NestedList a -> [a]
+flatten (Elem a) = [a]
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+flatten (List []) = []
+
+-- 8
+compress :: (Eq a) => [a] -> [a]
+compress []     = []
+compress [a]    = [a]
+compress (a:as) 
+    | elem a as = compress as
+    | otherwise = [a] ++ compress as

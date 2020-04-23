682516497b63b56660295d8b436adac18caa4713
Simon Tenggren <simon.tenggren@gmail.com>
2020-04-23 17:27:55 +0200

    work

--- n1/pattern.hs
+++ n1/pattern.hs
@@ -5,21 +5,29 @@ substitute wc (t:ts) s
           | wc == t = s ++ (substitute wc ts s)
           | otherwise = t : substitute wc ts s
 
-match :: Eq a => a -> [a] -> [a] ->  [a]
-match wc p  [] = [] 
-match wc [] s  = []
-match wc (p:pp) (s:ss)  
-            | s:ss == p:pp = []
-            | p == wc = takeUntil pp (s:ss)
-            | otherwise = match wc pp ss
+match :: Eq a => a -> [a] -> [a] -> Maybe [a]
+match wc p  [] = Nothing 
+match wc [] s  = Nothing 
 
--- singleWildcardMatch :: Eq a => a -> [a] -> [a] -> Maybe [a]
--- longerWildcardMatch :: Eq a => a -> [a] -> [a] -> Maybe [a]
+match wc (p:pp) s  
+            | p == wc = if (pp == tail (s))
+                            then singleWildcardMatch pp s
+                        else
+                            longerWildcardMatch (p:pp) s
+            | s == p:pp = Just []
+            | otherwise = match wc pp (tail s)
 
-takeUntil :: Eq a => [a] -> [a] -> [a]
-takeUntil _ [] = []
-takeUntil a b
-            | a == b = [] 
-            | otherwise = head b : takeUntil a (tail b)
-
+singleWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
+singleWildcardMatch [] _ = Nothing
+singleWildcardMatch _ [] = Nothing
+singleWildcardMatch (p:ps) (s:ss)
+            | match p (p:ps) (s:ss) == Nothing = Just [s] 
+            | otherwise = Nothing
+                    
+longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
+longerWildcardMatch _ [] = Nothing
+longerWildcardMatch [] _ = Nothing
+longerWildcardMatch (p:ps) (s:ss) 
+    | match p (p:ps) (s:ss) == Nothing =  Just (maybe [] (s :) (match p (p:ps) ss))
+    | otherwise = Just (maybe [] (s :) (match p (p:ps) ss))
 
